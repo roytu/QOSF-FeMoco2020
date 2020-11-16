457cd12260625c1396d3c0bfc2f1101e90ac40cd
Roy Tu <thatsnail@gmail.com>
2020-11-15 17:09:45 -0800

    Initial hamiltonian generation script

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Project for the QOSF mentorship program investigating the FeMo cofactor of nitro
 * (11/15/2020) Extracted ICS from [3U7Q](https://www.rcsb.org/3d-view/3U7Q/1) (see molecules/ICS.pdb)
     * This protein was found from [this paper](https://pubs.rsc.org/en/content/articlelanding/2019/cp/c8cp06930a#!divAbstract) and was chosen for being from a high-resolution x-ray diffraction (1.0 A) experiment and contained the correct atom (carbon) in the central cage.
 * (11/15/2020) Added ICS visualization from JMOL (see images/ics.png)
+* (11/15/2020) Added initial script to generate the Hamiltonian (see `gen_hamiltonian.py`)
---
+++ gen_hamiltonian.py
@@ -0,0 +1,88 @@
+import math
+import pickle
+import itertools
+import numpy as np
+import openfermion
+from pyscf import gto, scf, fci
+from copy import deepcopy
+from openfermion import QubitOperator
+from openfermion.hamiltonians import MolecularData
+from openfermionpyscf import run_pyscf
+from openfermion.transforms import get_fermion_operator, bravyi_kitaev, jordan_wigner
+from openfermion.utils import taper_off_qubits, commutator
+
+
+def load_xyz(xyz_fname):
+    """
+    Load a .xyz file into PySCF mol format
+
+    """
+
+    with open(xyz_fname, "r") as f:
+        lines = f.readlines()
+
+    # Remove header
+    lines = lines[2:]
+    output = []
+    for line in lines:
+        elems = line.split()
+
+        atom = elems[0].capitalize()
+        x = float(elems[1])
+        y = float(elems[2])
+        z = float(elems[3])
+
+        output.append([atom, [x, y, z]])
+
+    return output
+
+def get_qubit_hamiltonian(g, basis, charge=0, spin=1, qubit_transf='jw'):
+
+    mol = gto.Mole()
+    mol.atom = g
+    mol.basis = basis
+    mol.spin = spin
+    mol.symmetry = True
+    mol.max_memory = 1024
+    mol.build()
+
+    rhf = scf.RHF(mol)
+    rhf.kernel()
+    pickle.dump(rhf, open("rhf.pkl", "wb"))
+
+    1/0
+
+    cisolver = fci.FCI(mol, rhf.mo_coeff)
+    cisolver.kernel()
+    print(mol)
+
+    ham = mol.get_molecular_hamiltonian()
+    print(ham)
+    hamf = get_fermion_operator(ham)
+
+    if qubit_transf == 'bk':
+        hamq = bravyi_kitaev(hamf)
+    elif qubit_transf == 'jw':
+        hamq = jordan_wigner(hamf)
+    else:
+        raise(ValueError(qubit_transf, 'Unknown transformation specified'))
+
+    return remove_complex(hamq)
+
+def remove_complex(H : QubitOperator, tiny=1e-8):
+    '''
+    Removing near-zero complex coefficient
+    '''
+    real_h = QubitOperator.zero()
+    for term, val in H.terms.items():
+        if np.imag(val) < tiny:
+            val = np.real(val)
+        real_h += QubitOperator(term=term, coefficient=val)
+    return real_h
+
+
+if __name__ == "__main__":
+    g = load_xyz("molecules/ICS.xyz")
+    print(g)
+    H = get_qubit_hamiltonian(g, "sto-3g", charge=0, spin=4)
+    print(H)
